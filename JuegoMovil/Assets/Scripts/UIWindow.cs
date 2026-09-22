ab933ef65182f89bd45b0529f3cf446a772d1c5a
jemilio1407 <jemilio1407@gmail.com>
2026-09-22 16:42:22 -0600

    Subiendo UI con animacion

--- JuegoMovil/Assets/Scripts/UIWindow.cs
+++ JuegoMovil/Assets/Scripts/UIWindow.cs
@@ -1,16 +1,56 @@
+using DG.Tweening;
+using NaughtyAttributes;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIWindow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private RectTransform _canvasRectTransform;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private bool _hideOnStart;
+
+    [Header("Animation Settings")]
+    [SerializeField] private float showDuration = 0.5f;
+    [SerializeField] private float hideDuration = 0.5f;
+    [SerializeField] private Ease showEase = Ease.OutBack;
+    [SerializeField] private Ease hideEase = Ease.InBack;
     void Start()
     {
-        
+        Initialize();
+    }
+    public virtual void Initialize()
+    {
+        if(_hideOnStart)
+        {
+            Hide();
+        }
+    }
+    [Button]
+    public virtual void Show(bool instant = false)
+    {
+        if(instant)
+        {
+            _canvasRectTransform.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
+            rectTransform.DOScale(Vector3.one, duration:0.5f).SetEase(Ease.OutBack);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [Button]
+    public virtual void Hide(bool instant = false)
     {
-        
+        if (instant)
+        {
+            _canvasRectTransform.gameObject.SetActive(false);
+        }
+        else
+        {
+            RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
+            rectTransform.DOScale(Vector3.zero, duration: 0.5f).SetEase(Ease.InBack);
+        }
     }
 }
